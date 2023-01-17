9afc5b8a4460bd6786a43577fa36231e53f74c4c
swathi2346 <122012020+swathi2346@users.noreply.github.com>
2023-01-17 13:34:40 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,61 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 13,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "4\n",
+      "8\n",
+      "16\n",
+      "64\n",
+      "36\n",
+      "216\n",
+      "64\n",
+      "512\n",
+      "100\n",
+      "1000\n"
+     ]
+    }
+   ],
+   "source": [
+    "list=[2,4,6,8,10]\n",
+    "for i in list:\n",
+    "    print(i**2)\n",
+    "    print(i**3)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
